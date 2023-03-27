73ea7c472e69725bc3ba03490ec67c9f216d8284
Nour <nm.belsaadet@esi-sba.dz>
2023-03-27 15:44:23 +0100

    adding functions

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -22,3 +22,57 @@ db.toggleAccountStatus = async function toggleAccountStatus(req, res, next) {
         next(error);
     }
 }
+//@desc Assign Role To Account  
+//@route PUT /api/admin/Compte/:id 
+//@access Private 
+
+db.assignRoleToUser = async (req, res, next) => {
+    try {
+
+      if (req.roles.indexOf('ADMIN') === -1) {
+        const error = new Error("Vous n'êtes pas autorisé à effectuer cette opération.");
+        error.status = httpStatus.FORBIDDEN;
+        return next(error);
+      }
+  
+      const { idCompte, roles } = req.body;
+  
+      // Vérifie si le compte existe
+      const compte = await Compte.findByPk(idCompte);
+      if (!compte) {
+        const error = new Error(`Compte avec ID ${idCompte} n'existe pas.`);
+        error.status = httpStatus.NOT_FOUND;
+        return next(error);
+      }
+  
+      // Vérifie si les rôles existent
+      const _roles = await Role.findAll({
+        where: {
+          intitule: { 
+            [Op.in]: roles.split(',')
+          }
+        },
+        attributes: {
+          include: ['idRole']
+        }
+      });
+      if (_roles.length !== roles.split(',').length) {
+        const error = new Error('Un ou plusieurs rôles nexistent pas.');
+        error.status = httpStatus.NOT_FOUND;
+        return next(error);
+      }
+  
+      // Retire tous les rôles de compte
+     //   await compte.setRoles([]);
+  
+      // Affecte les nouveaux rôles au compte
+      await compte.setRoles(_roles);
+  
+      res.status(httpStatus.OK).json(
+        {message: `Les rôles ${roles} ont été assignés au compte ${idCompte} avec succès.`}
+      );
+  
+    } catch (error) {
+      next(error);
+    }
+  };
